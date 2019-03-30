dab6865f36e7ad969e9da05f64040b581a61bbef
scissorsfeet <scissorsfeet@163.com>
2019-03-30 18:31:06 +0800

    39. Combination Sum

---
+++ 39. Combination Sum/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates:[int], target: int) -> [[int]]:
+        def dfs(candidates, start, target, path, res):
+            if target == 0 :
+                return res.append(path + [])
+            for i in range(start, len(candidates)):
+                if target - candidates[i] >= 0:
+                    path.append(candidates[i])
+                    dfs(candidates, i, target-candidates[i], path, res)
+                    path.pop()
+
+        res = []
+        dfs(candidates, 0, target, [], res)
+        return res
+
+if __name__ == '__main__':
+  s = Solution()
+  print(s.combinationSum([2,3,6,7], 7))
+  print(s.combinationSum([1,100], 99))
